8f7f9bc6bd7afbc4162c6a04329588832b435574
Th3R1c0 <ricots114@protonmail.com>
2023-05-29 16:22:17 +1200

    header edi t

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import { FiSettings } from "react-icons/fi";
-import { TbReload } from "react-icons/Tb";
-// import { AiOutlineQuestionCircle } from "react-icons/Ai";
-import { IoExtensionPuzzleOutline } from "react-icons/io5";
+// import { FiSettings } from "react-icons/fi";
+// import { TbReload } from "react-icons/Tb";
+// // import { AiOutlineQuestionCircle } from "react-icons/Ai";
+// import { IoExtensionPuzzleOutline } from "react-icons/io5";
 const AppHeader = () => {
   return (
     <header className="w-screen p-4 bg-gray-400 text-2xl flex justify-between">
       <h1>Splatter AI Extreme Prototype </h1>
       <div className="flex items-center space-x-4 ">
-        <FiSettings />
+        {/* <FiSettings />
         <TbReload />
-        {/* <AiOutlineQuestionCircle /> */}
-        <IoExtensionPuzzleOutline />
+         <AiOutlineQuestionCircle /> 
+        <IoExtensionPuzzleOutline /> */}
       </div>
     </header>
   );
